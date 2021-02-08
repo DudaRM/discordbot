f331a28e5a882343d7b2a59d5c10e2e144e538b0
João Victor <joaovitormunizlopes@gmail.com>
2021-02-08 15:43:44 -0300

    Estabilishing Sqlite database and connection

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import os
 
 import discord
+import sqlite3
+from sqlite3 import Error
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,8 +10,22 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
+database_file = "botdatabase.db"
+
+def create_connection(db_file):
+    try:
+        conn=sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+
+conn = create_connection(database_file)
+
+
+
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(conn)
+    #print(f'{client.user} has connected to Discord!')
 
 client.run(TOKEN)
