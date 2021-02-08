236a1cfe6e76a485014d0dcb027a085ac644b6e5
João Victor <joaovitormunizlopes@gmail.com>
2021-02-08 17:50:15 -0300

    Inserting User

--- bot.py
+++ bot.py
@@ -21,11 +21,33 @@ def create_connection(db_file):
 
 conn = create_connection(database_file)
 
+def create_user(author):
+    try:
+        sql = 'INSERT INTO usuario(nome) VALUES(?)'
+        cur = conn.cursor()
+        cur.execute(sql,[str(author)])
+        conn.commit()
+        return cur.lastrowid
+
+    except Error as e:
+        print(e)
+
+title = 'Bot agenda'
+arg_missing_message = discord.Embed(title=title, description='Arguments are missing')
+blue_color = discord.Color.blue()
+gray_color = discord.Color.light_gray()
+red_color = discord.Color.red()
 
 
 @client.event
-async def on_ready():
-    print(conn)
-    #print(f'{client.user} has connected to Discord!')
+async def on_message(message):
+    
+    if message.author == client.user:
+        return
+
+    if message.content == ('botPlanner>addUser'):
+        idUser = create_user(message.author)
+        await message.channel.send(embed=(discord.Embed(title=title, description="Account created successfully your id is: "+str(idUser), color=blue_color)))
+        return
 
 client.run(TOKEN)
